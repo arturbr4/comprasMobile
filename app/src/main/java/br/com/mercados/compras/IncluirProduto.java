f8f6805e90a1275d835bf71122f0989271b3d16a
arturborges <aborges@effenti.ca>
2018-05-22 17:08:16 -0400

    Pagina de Inclusao

---
+++ app/src/main/java/br/com/mercados/compras/IncluirProduto.java
@@ -0,0 +1,13 @@
+package br.com.mercados.compras;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IncluirProduto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_incluir_produto);
+    }
+}
